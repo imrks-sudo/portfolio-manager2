ebe432954658ed8f0d6b54e30984d9f92b9d21a7
Rakesh Kumar <rakesh.kumar6@ingka.ikea.com>
2026-04-18 15:48:26 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://portfolio-manager2-27voijknf-imrks-sudos-projects.vercel.app"]
+  })
+);
 app.use(express.json());
 
 
